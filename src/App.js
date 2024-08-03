4ce9efa76c1b7ff001690b3516e073942faf8fd5
dnavadiya <navadiyadrashti8049@gmail.com>
2024-08-03 01:22:09 -0300

    modified folders

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import BlogPage from './pages/BlogPage';
+import UserProfilePage from './pages/UserProfilePage';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import NavbarComponent from './components/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import BlogPost from './components/BlogPost';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <NavbarComponent />
+        <div className="container mt-4">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/blog" element={<BlogPage />} />
+            <Route path="/profile" element={<UserProfilePage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path='/my-posts' element={<BlogPost />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
